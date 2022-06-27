a65c5a1acbbd03e5adf1add9610e0bbc46455166
Leonardo Flores <flores.leonardo@gmail.com>
2022-06-27 18:07:35 -0300

    Check dynamodb tables

--- maintainment/aws/dynamodb/check-table-data.sh
+++ maintainment/aws/dynamodb/check-table-data.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 ENVIRONMENT="POC"
-LIGHT_GREEN='\033[1;32m'
+RED='\033[0;31m'
 GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+LIGHT_RED='\033[1;31m'
+LIGHT_GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHT_BLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
 compare_environment_data() {
-    echo -e "Comparing ${GREEN}BCPR-$1-$3${NC} with ${GREEN}BCPR-$2-$3${NC}" 
-
     TABLE_ORIGIN=$(aws dynamodb scan --table-name BCPR-$1-$3)
     TABLE_DESTINATION=$(aws dynamodb scan --table-name BCPR-$2-$3)
 
     JSON_ORIGIN=$(jq -r '.Items' <<<$TABLE_ORIGIN)
     JSON_DESTINATION=$(jq -r '.Items' <<<$TABLE_DESTINATION)
 
-    diff <(jq --sort-keys . <<<$JSON_ORIGIN) <(jq --sort-keys . <<<$JSON_DESTINATION)
-
-    #compare_json_data $JSON_ORIGIN $JSON_DESTINATION
+    DIFF=$(diff <(jq --sort-keys . <<<$JSON_ORIGIN) <(jq --sort-keys . <<<$JSON_DESTINATION))
+    if [ "$DIFF" != "" ]; then
+        echo -e "${YELLOW}BCPR-$1-$3 and BCPR-$2-$3 are distinct ${NC}"
+        echo -e "${YELLOW}$DIFF${NC}"
+    else
+        echo -e "${LIGHT_GREEN}BCPR-$1-$3 and BCPR-$2-$3 are equals ${NC}"
+    fi
 }
 
 compare_json_data() {
@@ -23,7 +30,7 @@ compare_json_data() {
 }
 
 if [[ -n $3 ]]; then
-   TABLES=$(cat tables-data.json | jq -c ".[] | select( .name | contains(\"$3\"))")
+    TABLES=$(cat tables-data.json | jq -c ".[] | select( .name | contains(\"$3\"))")
     #echo $USER
     compare_environment_data \
         $1 \
@@ -35,8 +42,8 @@ fi
 TABLES=$(cat tables-data.json | jq -c -r '.[]')
 
 for TABLE in $TABLES; do
-  compare_environment_data \
-    $1 \
-    $2 \
-    $(jq -r '.name' <<<$TABLE)
+    compare_environment_data \
+        $1 \
+        $2 \
+        $(jq -r '.name' <<<$TABLE)
 done
